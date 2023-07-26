1059df9f234c36b5dd5fc3861f0d4af8e1d8dc86
xorsuyash <suyashsrivastavam87@gmail.com>
2023-07-26 22:21:25 +0530

    added all the neccessary functions

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn 
+
+from constants import EMBED_DIMENSION,EMBED_MAX_NORM
+
+
+class CBOW_Model(nn.Module):
+    
+    def __init__(self,vocab_size:int):
+        super(CBOW_Model,self).__init__()
+        
+        self.embeddings=nn.Embedding(num_embedding=vocab_size,
+                                     embedding_dim=EMBED_DIMENSION,
+                                     max_norm=EMBED_MAX_NORM,)
+        
+        self.linear= nn.Linear(
+            in_features=EMBED_DIMENSION,
+            out_features=vocab_size,
+        )
+        
+    def forward(self,x):
+        
+        x=self.embeddings(x)
+        x=x.mean(axis=1)
+        
+        x=self.linear(x)
+        
+        return x 

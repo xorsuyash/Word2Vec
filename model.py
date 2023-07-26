180943a0a17946669be21490ddbdc3401d7a06a6
xorsuyash <suyashsrivastavam87@gmail.com>
2023-07-26 22:35:48 +0530

    trained model

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class CBOW_Model(nn.Module):
     def __init__(self,vocab_size:int):
         super(CBOW_Model,self).__init__()
         
-        self.embeddings=nn.Embedding(num_embedding=vocab_size,
+        self.embeddings=nn.Embedding(num_embeddings=vocab_size,
                                      embedding_dim=EMBED_DIMENSION,
                                      max_norm=EMBED_MAX_NORM,)
         
@@ -26,3 +26,22 @@ class CBOW_Model(nn.Module):
         x=self.linear(x)
         
         return x 
+    
+class SkipGram_Model(nn.Module):
+  
+    def __init__(self, vocab_size: int):
+        super(SkipGram_Model, self).__init__()
+        self.embeddings = nn.Embedding(
+            num_embeddings=vocab_size,
+            embedding_dim=EMBED_DIMENSION,
+            max_norm=EMBED_MAX_NORM,
+        )
+        self.linear = nn.Linear(
+            in_features=EMBED_DIMENSION,
+            out_features=vocab_size,
+        )
+
+    def forward(self, inputs_):
+        x = self.embeddings(inputs_)
+        x = self.linear(x)
+        return x
